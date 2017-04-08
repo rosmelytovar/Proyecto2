14a77918e40437ed6145e4ca717b8c56ea63825f
Nayesia-PC\Nayesia <dayana.briceo39@gmail.com>
2017-04-08 18:58:33 -0400

    Main y ejecutable

--- main.cpp
+++ main.cpp
@@ -54,3 +54,87 @@ class cesar
 		int num;
 		string texto;		
 };
+int main() 
+{	cesar Texto;
+    string cadena,temp; 
+    int n=0,opc=0,bandera=0;
+    ofstream myfile;
+	do{
+		system("cls");
+		cout<<"\n ********************* " << endl;
+		cout<<"\n *        MENU       * " << endl;
+		cout<<"\n * 1. Codificar      * " << endl;
+		cout<<"\n * 2. Decodificar    * " << endl;
+		cout<<"\n * 3. Salir          * " << endl;
+		cout<<"\n * Digite su opcion  * " << endl;
+	    cout<<"\n ********************* " << endl;
+		cin>>opc;
+		fflush(stdin);
+		system("cls");
+		if (opc==1){ //CODIFICAR
+			cout << "Ingrese el mensaje a codificar" << endl;
+			cout << "estimado usuario al finalizar su mensaje debe escribir" <<endl;
+			cout << ":sacame para continuar el proceso"<<endl; 
+			cout << "es indiferente si es mayuscula o minuscula"<<endl; 
+			do{
+				
+		    	getline(cin, temp);
+		    	
+		    	if(temp==":SACAME" || temp==":sacame"){
+		    		bandera=1;
+		    		temp="";
+				}
+				cadena=cadena+temp+'\n';
+		    }while(bandera==0);
+		    
+		    for (int x=0; x < cadena.length(); x++){
+		    	if (cadena[x]>96 && cadena[x]<123){
+		    		cadena[x]=cadena[x]-32;
+				}
+			}	    	
+	    	cout << "Ingrese el numero de desplazamiento deseado" << endl; 
+	    	cin >> n;
+	    	Texto.setNum(n);
+	    	Texto.setCodifica(-n, cadena);
+	    	cout << Texto.getTexto() << endl;
+	    	system ("pause");
+	    	
+	    	 myfile.open("archivo", ios::trunc);
+	    	 myfile << Texto.getTexto();
+	    	 myfile.close();
+		}
+		if(opc==2){
+			cout << "Este es el texto codificado\n" << endl; 
+			ifstream archivoo ("archivo");
+		    string tex;
+		    while(getline(archivoo,tex)){
+		    	cout << tex << endl;
+			} 
+			archivoo.clear();
+			archivoo.seekg (0, ios::beg);
+			fflush(stdin);
+			cout << "\nComiense a Decodificar:\n" << endl; 
+		    cout << "Introduce el numero de desplazamiento deseado" << endl; 
+		    cin >> n;
+		    while(getline(archivoo,tex)){
+		    	Texto.setDescodifica(-n, tex);
+				cout << Texto.getTexto() << endl;
+				system ("pause");
+			}
+			
+		    archivoo.close();
+	}
+	           if(opc==3){
+					system("cls");
+					return 0;
+					system("pause");
+				 }
+				
+	
+	 
+	  } while(opc!='3');
+       
+       system("PAUSE");
+      
+}
+
